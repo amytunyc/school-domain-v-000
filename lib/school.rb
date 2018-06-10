6910d34a6ddc771be310d2b65e16ca577c55481a
Amy Tu <siamesebb@gmail.com>
2018-06-10 02:45:14 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -5,7 +5,12 @@ class School
     @roster = {}
   end
   
+  def roster
+    @roster
+  end
+    
   def add_student(name, grade)
     @student_name = name
-    @grade
+    @grade = grade
+  end
 end
