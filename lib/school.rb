b06e4410f6438ee74f56fd7e6c3cba236aaa19db
Amy Tu <siamesebb@gmail.com>
2018-06-10 02:46:51 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,5 +12,11 @@ class School
   def add_student(name, grade)
     @student_name = name
     @grade = grade
+    if @roster.include?(grade)==false
+      @roster[grade] = []
+    end
+    @roster[grade] << name
   end
+  
+  
 end
